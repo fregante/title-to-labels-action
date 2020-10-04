a7b93f149214f8031c0180d30db51f4d83785826
Federico Brigante <opensource@bfred.it>
2020-10-03 23:48:50 -0500

    Add support for PRs

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const core = require('@actions/core');
 const {Octokit} = require('@octokit/action');
 const parseTitle = require('./parse-title');
-const octokit = new Octokit();
 const event = require(process.env.GITHUB_EVENT_PATH);
+
+const octokit = new Octokit();
 
 function parseList(string) {
 	return string
-		.split(/[,\n]+/)
+		.split(/[\n,]+/)
 		.map(line => line.trim())
 		.filter(Boolean);
 }
 
 async function run() {
-	if (process.env.GITHUB_EVENT_NAME === 'issue') {
-		throw new Error('Only `issue` events are supported. Received: ' + process.env.GITHUB_EVENT_NAME);
+	if (!['issues', 'pull_request'].includes(process.env.GITHUB_EVENT_NAME)) {
+		throw new Error('Only `issues` and `pull_request` events are supported. Received: ' + process.env.GITHUB_EVENT_NAME);
 	}
 
-	if (['opened', 'edited'].includes(event.event)) {
-		throw new Error('Only `issue.opened` and `issue.edited` events are supported. Received: issue.' + event.event);
+	if (!['opened', 'edited'].includes(event.action)) {
+		throw new Error(`Only types \`opened\` and \`edited\` events are supported. Received: ${process.env.GITHUB_EVENT_NAME}.${event.action}`);
 	}
 
+	const conversation = event.issue || event.pull_request;
 	const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-	const issue_number = event.issue.number;
+	const issue_number = conversation.number;
 
 	const inputKeywords = parseList(core.getInput('keywords', {required: true}));
 	const inputLabels = parseList(core.getInput('labels'));
 	core.debug(`Received keywords: ${inputKeywords.join(', ')}`);
 	core.debug(`Received labels: ${inputLabels.join(', ')}`);
 
-	const {title, labels} = parseTitle(event.issue.title, {
+	const {title, labels} = parseTitle(conversation.title, {
 		keywords: inputKeywords,
 		labels: inputLabels
 	});
 
-	if (event.issue.title === title) {
+	if (conversation.title === title) {
 		core.info('No title changes needed');
 		return;
 	}
 
-	core.info(`Changing title from "${event.issue.title}" to ${title}`);
+	core.info(`Changing title from "${conversation.title}" to ${title}`);
 	core.info(`Adding labels: ${labels.join(', ')}`);
 
 	await Promise.all([
