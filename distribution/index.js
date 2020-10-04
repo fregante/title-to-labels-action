8a10808a55f771ef3ada0c1bfa8d6f887ea4ce14
Federico Brigante <opensource@bfred.it>
2020-10-04 17:50:59 -0500

    Restore usage with inputs

--- distribution/index.js
+++ distribution/index.js
@@ -24,7 +24,7 @@ function parseList(string) {
 		.filter(Boolean);
 }
 
-async function getInputs() {
+function getInputs() {
 	const keywords = parseList(core.getInput('keywords'));
 	const labels = parseList(core.getInput('labels'));
 	core.debug(`Received keywords: ${keywords.join(', ')}`);
